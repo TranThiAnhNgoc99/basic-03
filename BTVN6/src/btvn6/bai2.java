331bb3c9deae165f5a5a4992f2672bfedc9499f7
tranthianhngoc99 <tranminhngocpc@gmail.com>
2019-10-13 12:15:22 +0700

    BTVN6

--- BTVN6/src/btvn6/bai2.java
+++ BTVN6/src/btvn6/bai2.java
@@ -12,26 +12,27 @@ import java.util.Scanner;
  * @author pc
  */
 public class bai2 {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("nhap vao 1 so nguyen duong:");
-        int n=sc.nextInt();
-        int a=n;
-        int tongCS=0;
-        int donVi=0;
-        while(true){
-            if(a<1){
+        int n = sc.nextInt();
+        int a = n;
+        int tongCS = 0;
+        int donVi = 0;
+        while (true) {
+            if (a < 1) {
                 break;
-            }else{
-                donVi=a%10;
-                tongCS=tongCS +donVi;
-                a=a/10;
+            } else {
+                donVi = a % 10;
+                tongCS = tongCS + donVi;
+                a = a / 10;
             }
         }
-        System.out.println("tong cac chu so cua so vua nhap la: "+ tongCS);
-        if(n%tongCS==0){
+        System.out.println("tong cac chu so cua so vua nhap la: " + tongCS);
+        if (n % tongCS == 0) {
             System.out.println("so vua nhap la so dac biet!");
-        } else{
+        } else {
             System.out.println("so vua nhap khong la so dac biet!!!");
         }
     }
